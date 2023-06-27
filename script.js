4d8ae3dc494acf690b4e00d02a0d5c9aefce71c2
Daniel Alejandro Sanchez Paez <programazion1@gmail.com>
2023-06-27 01:52:24 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("Loading Javascript");
+
+function saveBook(){
+
+
+let nameBook = document.getElementById("name");
+let authorBook = document.getElementById("author");
+let isbnBook = document.getElementById("isbn");
+let editionBook = document.getElementById("edition");
+
+let Book ={
+    name:nameBook.value,
+    author:authorBook.value,
+    isbn:isbnBook.value,
+    edition:editionBook.value
+}
+
+console.log(Book);
+
+let url= "http://localhost:8000/api/Book";
+
+fetch(url,{
+    method:"POST",
+    headers:{
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(Book),
+})
+
+
+    alert("Creación de libro");
+
+    return true
+}
